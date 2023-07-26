438c150e2334de639a2d496d81a2a32ada5b9523
Kimleng <kimlenghor2006@gmail.com>
2023-07-26 09:48:24 -0500

    Initial Commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Kimleng Hor on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
